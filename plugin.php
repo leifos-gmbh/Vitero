ed984f50a9940a0f431560fd6fad67b786c09da6
smeyer-ilias <smeyer.ilias@gmx.de>
2018-03-28 11:57:40 +0200

    Increase plugin version

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = "xvit";
  
 // code version; must be changed for all code changes
-$version = "1.6.0";
+$version = "5.3.2.1";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.1.0";
-$ilias_max_version = "5.1.999";
+$ilias_min_version = "5.3.0";
+$ilias_max_version = "5.3.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Meyer";
